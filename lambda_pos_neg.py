07a73ca8224294a7bf7a09a8a763ca7e1bbcc767
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-11-22 11:07:24 -0300

    lambda_pos_neg.py

--- lambda_pos_neg.py
+++ lambda_pos_neg.py
@@ -1,7 +1,2 @@
-par_ou_impar = lambda numero : "par" if numero % 2 == 0 else "ímpar"
-
-numero = int(input("Digite um número: "))
-
-print(par_ou_impar(numero))
 
 pos_neg = lambda numero : "positivo" if numero > 0 else "negativo" if numero < 0 else "neutro"
