2a7e1ccb84b45f75c9dc344d5dbb2eb3d78c09c7
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-11-22 11:05:03 -0300

    concatena.py

--- quest2.py
+++ quest2.py
@@ -1,6 +1,8 @@
-concatena = lambda t1, t2 : f"{t1}{t2}" if len(t1) > 5 and len(t2) > 5 else "Erro"
+concatena = lambda texto1, texto2 : f"{texto1}{texto2}" if len(texto1) > 5 and len(texto2) > 5 else "Error"
 
-primeira = str(input("Digite a primeira palavra: "))
-segunda = str(input("Digite a segunda palavra: "))
 
-print(concatena(primeira, segunda))
+primeira_palavra = str(input("Digite a primeira palavra: "))
+segunda_palavra = str(input("Digite a segunda palavra: "))
+
+
+print(concatena(primeira_palavra, segunda_palavra))
