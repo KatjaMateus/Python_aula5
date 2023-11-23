ce66c964c3f66f7b322c4bb34229a2be51c5c897
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-11-22 22:19:31 -0300

    Add files via upload

---
+++ lambda_divisivel_por.py
@@ -0,0 +1,2 @@
+numero = lambda num : "Divisível" if num % 3 == 0 and num % 5 == 0 else "Não divisível"
+print(numero(300))
---
+++ lambda_maior_que10.py
@@ -0,0 +1,4 @@
+
+numero = lambda num : num if num > 10 else num / 2
+
+print(numero(90))
